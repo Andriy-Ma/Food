a0d7b43b5ee86a438469d990ad1c5dd38329f745
Andrii <www.andriy12@gmail.com>
2021-12-21 21:29:43 +0200

    Modal window

--- js/scriprt.js
+++ js/scriprt.js
@@ -94,4 +94,37 @@ window.addEventListener('DOMContentLoaded',() => {
     }
     setClock( '.timer', deadLine);
 
+    //Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalClose = document.querySelector('[data-close]');
+
+
+        modalTrigger.forEach(item =>{
+              item.addEventListener('click', openModal);
+            });
+
+        modalClose.addEventListener('click', closeModal );
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal){
+                closeModal();
+            }
+        });
+
+        document.addEventListener('keydown', (e) =>{
+            if (e.code === "Escape" ){
+                closeModal();
+            }
+        });
+
+        function closeModal() {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+        function openModal(){
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+          }
 });
